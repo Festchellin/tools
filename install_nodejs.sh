a279853e3db2e742ac6338ad353ef0a97968bfcc
Festchellin <festchellin@outlook.com>
2020-09-13 11:37:34 +0800

    分类配置各个安装脚本

---
+++ install_nodejs.sh
@@ -0,0 +1,29 @@
+# install wget curl net-tools
+sudo apt-get install wget curl net-tools -y
+# config nodejs
+wget -O node.tar.xz "https://nodejs.org/dist/v12.18.3/node-v12.18.3-linux-x64.tar.xz"
+# unzip the node.tar.xz
+tar -xvf node.tar.xz
+# rename the directory to node
+mv node-v12.18.3-linux-x64 node
+
+# move node to /opt/node
+sudo mv node /opt/node
+
+#config the PATH env
+echo "export NODE_HOME=/opt/node \
+	  export PATH=\$PATH:\$NODE_HOME/bin" >> ~/.zshrc
+
+# make changes update
+source ~/.zshrc
+
+# install nrm
+npm install -g nrm
+# config nrm repository
+nrm add taobao "http://registry.npm.taobao.org/"
+nrm add npm 	"https://registry.npmjs.org/"
+
+# use taobao repository
+nrm use taobao
+# install angular & react cli
+npm install -g @angular/cli create-react-app
---
+++ install_oh_my_zsh.sh
@@ -0,0 +1,6 @@
+sudo apt-get install git zsh -y
+# config the git username and email
+git config --global user.name "Festchellin"
+git config --global user.email "festchellin@outlook.com"
+
+bash oh_my_zsh_install.sh
